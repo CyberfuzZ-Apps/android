71a90dc675b16477e1a07e0703abca3001a598e1
CyberfuzZ <keepthelight@icloud.com>
2020-12-11 15:44:28 +0300

    Beer Adviser 1.1

--- BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -1,14 +1,33 @@
 package com.hfad.beeradviser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Вызывается когда пользователь кликает на кнопку
+    public void onClickFindBeer (View view) {
+
+        //Получить ссылку на TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //Получить ссылку на Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //Получить вариант выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Вывести выбранный вариант
+        brands.setText(beerType);
+
     }
 }
