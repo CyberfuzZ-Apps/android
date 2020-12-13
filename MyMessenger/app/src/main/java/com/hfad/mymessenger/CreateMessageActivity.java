acccab65e7c269727ad691a9eda4b983623140ef
CyberfuzZ <keepthelight@icloud.com>
2020-12-13 10:51:12 +0300

    Me messenger 1.0 вторая активность создана

--- MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -1,14 +1,20 @@
 package com.hfad.mymessenger;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class CreateMessageActivity extends AppCompatActivity {
+public class CreateMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
+    }
+
+    //Вызвать onSendMessage при щелчке на кнопке
+    public void onSendMessage(View view) {
+        
     }
 }
