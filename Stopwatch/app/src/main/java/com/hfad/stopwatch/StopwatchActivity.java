014c05b99b24d81b88d5547ac235e5f78e53cc0c
CyberfuzZ <keepthelight@icloud.com>
2020-12-13 19:44:47 +0300

    Stopwatch 1.0

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -2,17 +2,24 @@ package com.hfad.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StopwatchActivity extends Activity {
 
+    //Количество секунд на секундомере
     private int seconds = 0;
+    //Секундомер работает?
     private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
 
     //Старт секундомера, когда нажата кнопка Start
@@ -29,5 +36,29 @@ public class StopwatchActivity extends Activity {
     public void onClickReset(View view) {
         running = false;
         seconds = 0;
+    }
+
+    //Обновление показаний таймера
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+        })
     }
 }
