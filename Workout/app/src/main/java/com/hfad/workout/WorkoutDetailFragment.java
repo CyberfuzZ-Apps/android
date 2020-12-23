fca035c5ea155808f43b6a1a5e876493044fca3b
CyberfuzZ <keepthelight@icloud.com>
2020-12-23 16:11:27 +0300

    Workout 1.4 для телефонов и планшетов с сохранением данных при повороте

--- Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -1,19 +1,27 @@
 package com.hfad.workout;
 
 import android.os.Bundle;
-
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
 public class WorkoutDetailFragment extends Fragment {
 
     private long workoutId;
+
+    //Восстанавливается сохраненное значение workoutId при создании фрагмента
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +41,13 @@ public class WorkoutDetailFragment extends Fragment {
             TextView description = (TextView) view.findViewById(R.id.textDescription);
             description.setText(workout.getDescription());
         }
+    }
+
+    //Сохраняется текущее значение workoutId перед уничтожением фрагмента
+    //(Оно будет восстановлено в методе onCreate)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong("workoutId", workoutId);
     }
 
     public void setWorkout (long id) {
