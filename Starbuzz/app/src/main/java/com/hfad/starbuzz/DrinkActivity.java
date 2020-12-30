179681ed23250ab2624e8f20ea40e4ab34424f9f
CyberfuzZ <keepthelight@icloud.com>
2020-12-30 21:36:21 +0300

    Starbuzz 1.4 - списки, описания и фото берутся из базы данных SQLite

--- Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -1,9 +1,14 @@
 package com.hfad.starbuzz;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkActivity extends Activity {
 
@@ -16,19 +21,49 @@ public class DrinkActivity extends Activity {
 
         //Получить напиток из данных интента
         int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
-        Drink drink = Drink.drinks[drinkId];
 
-        //Заполнение названия напитка
-        TextView name = (TextView) findViewById(R.id.name);
-        name.setText(drink.getName());
 
-        //Заполнение описания напитка
-        TextView description = (TextView) findViewById(R.id.description);
-        description.setText(drink.getDescription());
+        //Создание курсора
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("DRINK",
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    "_id = ?",
+                    new String[] {Integer.toString(drinkId)},
+                    null, null, null);
 
-        //Заполнение изображения напитка
-        ImageView photo = (ImageView) findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResourceId());
-        photo.setContentDescription(drink.getName());
+            //Переход к первой записи в курсоре
+            if (cursor.moveToFirst()) {
+
+                //Получение данных напитка из курсора
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+
+                //Заполнение названия напитка
+                TextView name = (TextView) findViewById(R.id.name);
+                name.setText(nameText);
+
+                //Заполнение описания напитка
+                TextView description = (TextView) findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                //Заполнение изображения напитка
+                ImageView photo = (ImageView) findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+            }
+
+            cursor.close();
+            db.close();
+
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this,
+                    "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
     }
 }
