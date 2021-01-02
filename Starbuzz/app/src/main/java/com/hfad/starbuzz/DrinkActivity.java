5519c2bbaff5bd519d49d483016222b50fd7a4da
CyberfuzZ <keepthelight@icloud.com>
2021-01-02 13:34:35 +0300

    Starbuzz 1.5 - добавлен флажок "Favorite", столбец FAVORITE в базу данных и возможность обновления базы данных

--- Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkActivity.java
@@ -1,11 +1,14 @@
 package com.hfad.starbuzz;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,7 +31,7 @@ public class DrinkActivity extends Activity {
         try {
             SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
             Cursor cursor = db.query("DRINK",
-                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID", "FAVORITE"},
                     "_id = ?",
                     new String[] {Integer.toString(drinkId)},
                     null, null, null);
@@ -40,6 +43,7 @@ public class DrinkActivity extends Activity {
                 String nameText = cursor.getString(0);
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
+                boolean isFavorite = (cursor.getInt(3) == 1);
 
                 //Заполнение названия напитка
                 TextView name = (TextView) findViewById(R.id.name);
@@ -53,6 +57,10 @@ public class DrinkActivity extends Activity {
                 ImageView photo = (ImageView) findViewById(R.id.photo);
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
+
+                //Заполнение флажка любимого напитка
+                CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
             }
 
             cursor.close();
@@ -63,7 +71,30 @@ public class DrinkActivity extends Activity {
                     "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
+    }
 
+    //Обновление базы данных по щелчку на флажке
+    public void onFavoriteClicked(View view) {
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
 
+        //Получение значения флажка
+        CheckBox favorite = (CheckBox) findViewById(R.id.favorite);
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("FAVORITE", favorite.isChecked());
+
+        //Получение ссылки на базу данных и обновление столбца FAVORITE
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+            db.update("DRINK",
+                    drinkValues,
+                    "_id = ?",
+                    new String[] {Integer.toString(drinkId)});
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
