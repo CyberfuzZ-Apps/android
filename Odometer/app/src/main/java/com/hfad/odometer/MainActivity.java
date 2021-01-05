1a842229a236e6b30e51f70655be5e5ad8c45e7b
CyberfuzZ <keepthelight@icloud.com>
2021-01-05 18:06:51 +0300

    Odometer 1.1 - запрашивает разрешение на использование геопозиции и использует ее

--- Odometer/app/src/main/java/com/hfad/odometer/MainActivity.java
+++ Odometer/app/src/main/java/com/hfad/odometer/MainActivity.java
@@ -1,11 +1,18 @@
 package com.hfad.odometer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -17,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
     private OdometerService odometer;
     private boolean bound = false;
+    private final int PERMISSION_REQUEST_CODE = 698;
+    private final int NOTIFICATION_ID = 423;
 
-    //Создание объекта ServiceConnection
+    //Создание объекта ServiceConnection для связывания MainActivity c OdometerService
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder binder) {
@@ -50,9 +59,62 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, OdometerService.class);
-        //Связывание службы выполняется при запуске активности
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+
+        //Если разрешение на геолокацию еще не было предоставлено
+        if (ContextCompat.checkSelfPermission(this,
+                OdometerService.PERMISSION_STRING) !=
+                PackageManager.PERMISSION_GRANTED) {
+            //запросить его во время выполнения
+            ActivityCompat.requestPermissions(this,
+                    new String[]{OdometerService.PERMISSION_STRING},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+            //Если разрешение уже было предоставлено - выполнить
+            //связывание со службой
+            Intent intent = new Intent(this, OdometerService.class);
+            //Связывание службы выполняется при запуске активности
+            bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                } else {
+                    //Создание уведомления при отказе от геолокации
+                    NotificationCompat.Builder builder =
+                            new NotificationCompat.Builder(this)
+                            .setSmallIcon(android.R.drawable.ic_menu_compass)
+                            .setContentTitle(getResources().getString(R.string.app_name))
+                            .setContentText(getResources().getString(R.string.permission_denied))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setVibrate(new long[] {1000, 1000})
+                            .setAutoCancel(true);
+
+                    //Создание действия
+                    Intent actionIntent = new Intent(this,
+                            MainActivity.class);
+                    PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                            this,
+                            0,
+                            actionIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(actionPendingIntent);
+
+                    //Выдача уведомления
+                    NotificationManager notificationManager =
+                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+                }
+            }
+        }
     }
 
     @Override
